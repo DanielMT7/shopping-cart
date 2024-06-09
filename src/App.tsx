b1fcea3f64b747ad54f18d66c7e2ff0a48b663c7
Daniel Motta <danielmotta04@hotmail.com>
2024-06-09 19:11:13 -0300

    refactor: add provider to app content

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 import Navbar from './components/Navbar/Navbar'
+import { ShoppingCartProvider } from './context/ShoppingCartContext'
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
         <Outlet />
       </Container>
-    </>
+    </ShoppingCartProvider>
   )
 }
 
