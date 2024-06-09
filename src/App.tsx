a30d19d61466f0d659cfb42b791e703d8811685b
Daniel Motta <danielmotta04@hotmail.com>
2024-06-09 16:16:49 -0300

    refactor: add Navbar and Outlet to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Outlet } from 'react-router-dom'
+
+import { Container } from 'react-bootstrap'
+
+import Navbar from './components/Navbar/Navbar'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Outlet />
+      </Container>
+    </>
+  )
 }
 
 export default App
