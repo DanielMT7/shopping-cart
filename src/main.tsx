dfb6a08f29756b1a970d72f4d18a25ac56b9e101
Daniel Motta <danielmotta04@hotmail.com>
2024-06-09 15:53:19 -0300

    feature: add new routes for home, about and store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.tsx'
+import Home from './pages/Home/Home.tsx'
+import About from './pages/About/About.tsx'
+import Store from './pages/Store/Store.tsx'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/store',
+        element: <Store />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
